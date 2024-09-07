d5ecf315ef835cc83185654fec456c405faef1d8
Renin56 <kse.renin@yandex.ru>
2024-09-07 09:20:53 +0500

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,7 @@
+completed_homework = 12
+hours_used = 1.5
+name_course = "Python"
+time_for_one_task = 1.5 / 12
+print(name_course + ', всего задач: ' + str(completed_homework) +
+                    ', затрачено часов: ' + str(hours_used) +
+                    ', среднее время выполнения ' + str(time_for_one_task) + ' часа')
